6e1bdabef3fe37383b4417324aae8c73da9a71df
NelsonEspadoto <nelsonespadoto@gmail.com>
2020-09-25 12:15:28 -0300

    atualizando rotas

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class ConfigController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $data = $request->all();
+        $dados = $request->input('nome');
+
+        echo 'Seja Bem Vindo '.$data['nome'];
+        print_r($dados);
+
         return view('config');
     }
 
